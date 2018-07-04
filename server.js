fac25d3bd50c0ee430b8e17ee02e389e3ce6eb06
Nickolas Fuentes <nickolasfuentes@Mark-1.attlocal.net>
2018-07-03 21:47:55 -0500

    Added middleware, hbs and fs to have a log of the request on sites

--- server.js
+++ server.js
@@ -1,20 +1,51 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
 
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (error) => {
+        if(error) {
+        console.log('Unable to append to server.log.')
+        } else {
+        next();
+        }
+    });
+});
+
+// app.use((req, res, next)=> {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+   return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-    // res.send('<h1>Hello Express!</h1>');
-    res.send({
-        name: 'Nickolas',
-        likes: [
-            'Coding',
-            'Basketball'
-        ]
-    })
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website'
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
 });
 
 app.get('/bad', (req, res) => {
@@ -23,4 +54,6 @@ app.get('/bad', (req, res) => {
     })
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
